c557ee5528db9d04e3af036efea387adb14cb0ce
wullb <jarvisroot@outlook.com>
2025-03-07 14:22:32 +0800

    init

--- internal/testing/mocks/author.ts
+++ internal/testing/mocks/author.ts
@@ -3,6 +3,6 @@ import contacts from "./contacts";
 export default {
   title: "Forzen Wu",
   photo: "/logo.png",
-  description: "Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.",
+  description: "1Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.",
   contacts,
 };
